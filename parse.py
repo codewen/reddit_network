c5fa97b7cf078120f2c2b01385c655ab9c752aa4
codewen <codewen@gmail.com>
2019-05-09 04:43:33 +1000

    as

--- parse.py
+++ parse.py
@@ -10,24 +10,40 @@ def readBody():
             array.append(row)
 
         # select the colum
-        toArr = list(map(lambda x:x[2], array))
+        fromArr = list(map(lambda x:x[0], array))
+        toArr = list(map(lambda x:x[1], array))
+        IDArr = list(map(lambda x:x[2], array))
+        TimeArr = list(map(lambda x:x[3], array))
         sentimentArr = list(map(lambda x:x[4], array))
+        propArr = list(map(lambda x:x[5].split(',')[0], array))
 
+        fromArr.pop(0)
         toArr.pop(0)
+        IDArr.pop(0)
+        TimeArr.pop(0)
         sentimentArr.pop(0)
+        propArr.pop(0)
 
         
-        df = pd.DataFrame({'to':toArr,'sentiment':sentimentArr})
+        df = pd.DataFrame({
+            # 'from':fromArr,
+            # 'to':toArr,
+            'ID':IDArr,
+            # 'Time':TimeArr,
+            # 'sentiment':sentimentArr,
+            'prop':propArr
+        })
+        print(df.shape)
         
-        is_minus_1 =  df['sentiment']=='-1'
+        is_minus_1 =  df['ID']=='3yj2ee'
+
 
         df = df[is_minus_1]
-        df = df.groupby(['to']).count()
-        df = df.sort_values(by=['sentiment'], ascending=False)
+        print(df)
+        # df = df.groupby(['to']).count()
+        # df = df.sort_values(by=['sentiment'], ascending=False)
 
-
-        print(df.head(300))
-        print(df.shape)
+        # print(df.shape)
 
 
 
